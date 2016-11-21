f5a2b170aff81d069b6b1ebdce9bf9105e47ada4
Sylvain Cristofari <s.cristofari@gmail.com>
2016-11-21 11:28:08 +0100

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 
-import Home from './modules/Home/Home';
-import About from './modules/About/About';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Header from './components/Header/Header';
 
-import './index.css';
+import '../node_modules/foundation-sites/dist/foundation-flex.css';
 
 ReactDOM.render(
-  <Router history={browserHistory}>
-    <Route path="/" component={Home}/>
-    <Route path="/about" component={About}/>
-  </Router>
+  <div className="layout">
+    <Header />
+    <Router history={browserHistory}>
+      <Route path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+    </Router>
+  </div>
   , document.getElementById('root')
 )
