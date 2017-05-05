c197c0c82380b1148fd181e814c83a1e0e93d1cb
Sylvain Cristofari <s.cristofari@gmail.com>
2017-05-05 12:30:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,6 @@
 import firebase from 'firebase'
 
-const config = {
-    apiKey: "AIzaSyDiHd5WgcdgPAKhfN5-1VoweaC_tsvX3Zw",
-    authDomain: "socialtv-4e099.firebaseapp.com",
-    databaseURL: "https://socialtv-4e099.firebaseio.com",
-    storageBucket: "socialtv-4e099.appspot.com",
-    messagingSenderId: "357983380946"
-};
+const config = {};
 
 export const firebaseApp = firebase.initializeApp(config)
 export const firebaseAuth = firebaseApp.auth()
